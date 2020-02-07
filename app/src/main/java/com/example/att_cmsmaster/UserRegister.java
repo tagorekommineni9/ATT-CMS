07945ed8365a46173f62106f2b4195fe7291c8d1
arshhd014 <arshhd014@gmail.com>
2020-02-07 07:09:01 +0530

    Collaboration Test- Arshdeep

---
+++ app/src/main/java/com/example/att_cmsmaster/UserRegister.java
@@ -0,0 +1,16 @@
+package com.example.att_cmsmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+
+        //Collaboration Test
+    }
+}
