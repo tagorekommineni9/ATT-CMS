4cdddc551d10d6b29da2c79ac27663ae20d63bc7
tanyachawla01 <tanyachawla2227@gmail.com>
2020-02-07 18:49:29 -0500

    1) StaffRegister Activity to StaffLogin Activity 2) Views added

--- app/src/main/java/com/example/att_cmsmaster/StaffRegister.java
+++ app/src/main/java/com/example/att_cmsmaster/StaffRegister.java
@@ -2,9 +2,23 @@ package com.example.att_cmsmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StaffRegister extends AppCompatActivity {
+    EditText staffId;
+    EditText fullname;
+    EditText email;
+    EditText phone;
+    EditText password;
+    EditText confirmPassword;
+
+    Button registerButton;
+    TextView loginQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +26,24 @@ public class StaffRegister extends AppCompatActivity {
         setContentView(R.layout.activity_staff_register);
 
         //Collaboration Test - Tanya
+
+        staffId = findViewById(R.id.staff_id);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+
+        registerButton = findViewById(R.id.regButton);
+        loginQuestion = findViewById(R.id.loginQuestion);
+
+
+        loginQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(),StaffLogin.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
