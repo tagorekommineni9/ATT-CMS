bd859cba3b8be7ecbe729936cca604068ce36913
arshhd014 <arshhd014@gmail.com>
2020-02-08 05:05:58 +0530

    1)Added Views 2)Intent to go from UserRegister Activity to UserLogin Activity

--- app/src/main/java/com/example/att_cmsmaster/UserLogin.java
+++ app/src/main/java/com/example/att_cmsmaster/UserLogin.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class UserLogin extends AppCompatActivity {
+
+
 
     TextView userRegisterQuestion;
     TextView staffLoginQuestion;
@@ -16,6 +20,8 @@ public class UserLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+;
 
         userRegisterQuestion = findViewById(R.id.userRegisterQuestion);
         staffLoginQuestion = findViewById(R.id.staffLoginQuestion);
