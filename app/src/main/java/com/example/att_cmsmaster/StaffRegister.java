c15c10c63aadc68f7b5e04763eb2d423527c8548
tanyachawla01 <tanyachawla2227@gmail.com>
2020-02-07 11:44:03 -0500

    Test -tanya

---
+++ app/src/main/java/com/example/att_cmsmaster/StaffRegister.java
@@ -0,0 +1,16 @@
+package com.example.att_cmsmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaffRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_register);
+
+        //Collaboration Test - Tanya
+    }
+}
