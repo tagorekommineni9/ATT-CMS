3a67cc1139cd9771844cee666d3aa64fd7fb5025
tanyachawla01 <tanyachawla2227@gmail.com>
2020-02-07 19:09:39 -0500

    1) Views Added 2) Intent(StaffLogin Activity to StaffRegister Activity)

--- app/src/main/java/com/example/att_cmsmaster/StaffLogin.java
+++ app/src/main/java/com/example/att_cmsmaster/StaffLogin.java
@@ -2,13 +2,38 @@ package com.example.att_cmsmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StaffLogin extends AppCompatActivity {
+    EditText adminID;
+    EditText adminPassword;
+
+    Button adminLoginButton;
+    TextView staffRegisterQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_login);
+
+        adminID = findViewById(R.id.adminID);
+        adminPassword = findViewById(R.id.adminPass);
+
+        adminLoginButton = findViewById(R.id.adminLgnButton);
+        staffRegisterQuestion = findViewById(R.id.staff_reg_question);
+
+        staffRegisterQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent staffRegIntent = new Intent(getApplicationContext(), StaffRegister.class);
+                startActivity(staffRegIntent);
+
+            }
+        });
     }
 }
