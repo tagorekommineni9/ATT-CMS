cca63f4034fbe3fe4b99df4a76fac55ee126cb3a
tagorekommineni9 <tagorek7@kluniversity.in>
2020-02-12 00:59:23 -0500

    User Dashboard screen added

---
+++ app/src/main/java/com/example/att_cmsmaster/UserDashboard.java
@@ -0,0 +1,14 @@
+package com.example.att_cmsmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+    }
+}
