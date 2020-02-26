35c86993167f3c3e9c4504818a37d8bebe10abf1
tagorekommineni9 <tagorek7@kluniversity.in>
2020-02-26 09:30:48 -0500

    Registering the user in firebase using Firebase Authentication

--- app/src/main/java/com/example/att_cmsmaster/UserRegister.java
+++ app/src/main/java/com/example/att_cmsmaster/UserRegister.java
@@ -1,22 +1,42 @@
 package com.example.att_cmsmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserRegister extends AppCompatActivity {
 
-    EditText fullname;
-    EditText email;
-    EditText phone;
-    EditText password;
-    EditText confirmpassword;
+    EditText mfullname;
+    EditText memail;
+    EditText mphone;
+    EditText mpassword;
+    EditText mconfirmpassword;
 
     TextView userLoginQuestion;
+
+    Button userRegister;
+
+    FirebaseAuth  fAuth;
+    ProgressBar progressBar;
+
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+   // DatabaseReference myRef = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +45,68 @@ public class UserRegister extends AppCompatActivity {
 
         setTitle("User Registration");
 
-        fullname = findViewById(R.id.fullname);
-        email = findViewById(R.id.email);
-        phone = findViewById(R.id.phone);
-        password = findViewById(R.id.password);
-        confirmpassword = findViewById(R.id.confirmPassword);
+        mfullname = findViewById(R.id.fullname);
+        memail = findViewById(R.id.email);
+        mphone = findViewById(R.id.phone);
+        mpassword = findViewById(R.id.password);
+        mconfirmpassword = findViewById(R.id.confirmPassword);
+
+        userRegister = findViewById(R.id.regButton);
 
         userLoginQuestion = findViewById(R.id.loginQuestion);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBarUserRegister);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity( new Intent(getApplicationContext(),UserDashboard.class));
+            finish();
+        }
+
+        userRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    memail.setError("Email is Required!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Password is Required!");
+                    return;
+                }
+
+                if(password.length() <6){
+                    mpassword.setError("Password must be at least 6 characters");
+                    return;
+                }
+
+                if(!mpassword.getText().toString().equals(mconfirmpassword.getText().toString())){
+                    mconfirmpassword.setError("Please make sure your passwords match");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // regsitering the user in firebase
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(UserRegister.this," Yay! You are registered.",Toast.LENGTH_SHORT).show();
+                            startActivity( new Intent(getApplicationContext(),UserDashboard.class));
+                        }
+                        else{
+                            Toast.makeText(UserRegister.this," Error ! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         userLoginQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,4 +117,13 @@ public class UserRegister extends AppCompatActivity {
         });
 
     }
+
+
+  /*  private void writeNewUser(String userId, String name, String email, String phone, String password) {
+        User user = new User(name, email, phone, password);
+
+        myRef.child("users").child(userId).setValue(user);
+    }  */
+
+
 }
