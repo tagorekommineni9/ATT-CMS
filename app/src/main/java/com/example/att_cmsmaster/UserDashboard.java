2c4334760e5cd7f67d2d56fe6f82fe2b7736132c
arshhd014 <arshhd014@gmail.com>
2020-02-26 23:39:28 +0530

    1) Changed layout in UserDashboard XML file 2) Signout added for for UserDashboard

--- app/src/main/java/com/example/att_cmsmaster/UserDashboard.java
+++ app/src/main/java/com/example/att_cmsmaster/UserDashboard.java
@@ -2,7 +2,11 @@ package com.example.att_cmsmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserDashboard extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class UserDashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+    }
+
+    public void userlogout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity( new Intent(getApplicationContext(),UserLogin.class));
+        finish();
     }
 }
