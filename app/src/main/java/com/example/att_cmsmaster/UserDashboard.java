b798e5318145151c47128a321e68d1919f4f93ed
tagorekommineni9 <tagorek7@kluniversity.in>
2020-03-05 23:24:25 -0500

    Added Code to navigate from UserDashboard

--- app/src/main/java/com/example/att_cmsmaster/UserDashboard.java
+++ app/src/main/java/com/example/att_cmsmaster/UserDashboard.java
@@ -5,15 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class UserDashboard extends AppCompatActivity {
 
+    TextView userProfileSettings,userRegisterComplaint ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
+
+        userProfileSettings = findViewById(R.id.textViewUserProfileSettings);
+        userRegisterComplaint = findViewById(R.id.textViewRegisterComplaint);
+
+        userProfileSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserDashboard.this,EditProfile.class));
+            }
+        });
+
+        userRegisterComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserDashboard.this,RegisterComplaint.class));
+            }
+        });
     }
 
     public void userlogout(View view) {
